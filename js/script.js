18b06a0444b49be3065d2e0d2123f0fc464115be
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-03-15 10:50:00 +0100

    [add] chevron display at hover and [add] active class on contact clicked

--- js/script.js
+++ js/script.js
@@ -45,6 +45,26 @@ createApp({
         messageItem.isMessageClicked = true;
       }
     },
+
+    deleteMessage(activeContactId, messageItemId) {
+      console.log("predelete message");
+      console.log("messageItemId", messageItemId);
+      console.log(
+        "array length",
+        this.contactsFiltered[activeContactId].messages.length
+      );
+
+      if (this.contactsFiltered[activeContactId].messages.length === 1) {
+        console.log("ultimo messaggio");
+        this.contactsFiltered[activeContactId].messages = ["array vuoto"];
+        // this.contactsFiltered[activeContactId].messages.length = 0;
+      } else {
+        this.contactsFiltered[activeContactId].messages.splice(
+          messageItemId,
+          1
+        );
+      }
+    },
   },
 
   computed: {
